39afa14ff700d6d3f7e62b389222c56217b3d0ce
Biggest.Deb <alextross15@gmail.com>
2022-08-10 19:24:00 -0400

    Updated overall look, added clear button and h1.

--- thingsHappenHere.js
+++ thingsHappenHere.js
@@ -1,6 +1,7 @@
 let container = document.querySelector("#container");
-let columnLimit = 16;
-let cellLimit = 16;
+let button = document.querySelector("#btn");
+let columnLimit = 32;
+let cellLimit = 32;
 
 for( let i = 0; i < columnLimit; i++){
     let column = document.createElement('div');
@@ -11,5 +12,24 @@ for( let i = 0; i < columnLimit; i++){
         let cell = document.createElement('div');
         cell.classList.add("cell");
         column.appendChild(cell);
+
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });
     }
 }
+
+button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = "grey";
+});
+
+button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = "black";
+});
+
+button.addEventListener('click', () => {
+    location.reload();
+});
+
+ 
+
